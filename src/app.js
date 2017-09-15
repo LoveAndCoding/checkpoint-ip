735575397b5b519ca8acd6ffe488ac78e05109d5
Andrew Hushbeck <andrew.hushbeck@imgineme.com>
2017-09-14 23:13:56 -0700

    Socket Request/Response Service (Messy Version)
    Just getting the first implementation up. Needs clean up and work, but this is the start of things for the basic outline.

--- src/app.js
+++ src/app.js
@@ -1,27 +1,76 @@
 'use strict';
 
 const cluster = require('cluster');
-const http = require('http');
+const net = require('net');
 const numCPUs = require('os').cpus().length;
+
+const load = require('./load');
+
+function sendToWorkers(data) {
+	for(let id in cluster.workers) {
+		cluster.workers[id].send(data);
+	}
+}
 
 if (cluster.isMaster) {
 	console.log(`Master ${process.pid} is running`);
 
-	// Fork workers.
-	for (let i = 0; i < numCPUs; i++) {
-		cluster.fork();
-	}
-
-	cluster.on('exit', (worker, code, signal) => {
-		console.log(`worker ${worker.process.pid} died`);
-	});
+	load('./firehol_level1.netset', 'Firehol Level 1')
+		.then((ips) => {
+			// Fork workers.
+			for (let i = 0; i < numCPUs; i++) {
+				cluster.fork();
+			}
+			
+			sendToWorkers(ips);
+			
+			cluster.on('exit', (worker, code, signal) => {
+				console.log(`worker ${worker.process.pid} died`);
+			});
+		})
+		.then(null, (err) => {
+			console.error(err);
+		});
+	
 } else {
-	// Workers can share any TCP connection
-	// In this case it is an HTTP server
-	http.createServer((req, res) => {
-		res.writeHead(200);
-		res.end('hello world\n');
-	}).listen(8000);
+	const ip = require('ip');
+	
+	let ipMap = Object.create(null);
+	
+	cluster.worker.on('message', (data) => {
+		ipMap = data;
+	});
+	
+	net.createServer((connection) => {
+		connection.setEncoding('utf8');
+		
+		function checkIp(address) {
+			const parts = address.split('.');
+			let curr = ipMap;
+			
+			while(typeof curr === 'object' && curr) {
+				curr = curr[parts.shift()];
+			}
+			connection.write(JSON.stringify({
+				ip: address,
+				allowed: !curr,
+				list: curr,
+			}) + '\n');
+		}
+		
+		let currChunk = '';
+		connection.on('data', (chunk) => {
+			currChunk += chunk;
+			
+			while(currChunk.indexOf('\n') >= 0) {
+				let idx = currChunk.indexOf('\n');
+				let addr = currChunk.substring(0, idx);
+				checkIp(addr);
+				currChunk = currChunk.slice(idx + 1);
+			}
+		});
+		
+	}).listen(process.env.PORT || 8000);
 
 	console.log(`Worker ${process.pid} started`);
 }
